d26b22e77b79c0f7e5eced8fde478f1b08a77ab7
Firat Tale <firat.tale@auto1.com>
2022-04-07 13:18:54 +0200

    Remove Ethereum Logo and Links

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -3,8 +3,7 @@ import { Contract } from "@ethersproject/contracts";
 import { shortenAddress, useCall, useEthers, useLookupAddress } from "@usedapp/core";
 import React, { useEffect, useState } from "react";
 
-import { Body, Button, Container, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+import { Body, Button, Container, Header } from "./components";
 
 import { addresses, abis } from "@my-app/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
@@ -55,7 +54,8 @@ function App() {
 			method: "balanceOf",
 			args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
 		}) ?? {};
-
+	console.log("contractCallError", contractCallError);
+	console.log("tokenBalance", tokenBalance);
 	const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
 
 	useEffect(() => {
@@ -74,13 +74,6 @@ function App() {
 				<WalletButton />
 			</Header>
 			<Body>
-				<Image src={logo} alt="ethereum-logo" />
-				<p>
-					Edit <code>packages/react-app/src/App.js</code> and save to reload.
-				</p>
-				<Link href="https://reactjs.org">Learn React</Link>
-				<Link href="https://usedapp.io/">Learn useDapp</Link>
-				<Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
 				<Button>Mint NFT</Button>
 			</Body>
 		</Container>
