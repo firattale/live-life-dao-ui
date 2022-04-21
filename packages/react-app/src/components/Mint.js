5c31b267c42644cb31c2f3053e4ef793b6494809
Lorelai <nadja.rathenow@rub.de>
2022-04-21 19:31:12 +0200

    mintpage merging desktop and mobile components

--- packages/react-app/src/components/Mint.js
+++ packages/react-app/src/components/Mint.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-import { Container, ContainerMint } from ".";
+import { ContainerMint, Container } from ".";
 import MintIntro from "./MintPage/MintIntro";
 import Golden from "./MintPage/Golden";
 import Guestlist from "./MintPage/Guestlist";
-
-import GoldenDesktop from "./MintPage/GoldenDesktop";
-import GuestlistDesktop from "./MintPage/GuestlistDesktop";
 import NftImage from "./MintPage/NftImage";
 
 export default function MintPage({ onGuestListClick, onGoldenClick }) {
+
 	const [width, setWidth] = useState(window.innerWidth);
 	const breakpoint = 1000;
 
@@ -22,23 +20,18 @@ export default function MintPage({ onGuestListClick, onGoldenClick }) {
 		return () => window.removeEventListener("resize", updateWidth);
 	}, []);
 
-	if (width > breakpoint) {
-		return (
-			<Container>
-				<div className="mint-container-desktop padding-nft-heading">
-					<GoldenDesktop onGoldenClick={onGoldenClick} />
-					<GuestlistDesktop onGuestListClick={onGuestListClick} />
-				</div>
-				<NftImage />
-			</Container>
-		);
-	}
-
 	return (
-		<ContainerMint>
-			<MintIntro />
-			<Golden onGoldenClick={onGoldenClick} />
-			<Guestlist onGuestListClick={onGuestListClick} />
-		</ContainerMint>
+		<Container>
+			<ContainerMint>
+				{width < breakpoint
+				&&  <MintIntro />
+				}
+				<Golden onGoldenClick={onGoldenClick}/>
+				<Guestlist onGuestListClick={onGuestListClick}/>
+			</ContainerMint>	
+			{width > breakpoint
+				&&  <NftImage />
+			}
+		</Container>
 	);
 }
--- packages/react-app/src/components/MintPage/Golden.js
+++ packages/react-app/src/components/MintPage/Golden.js
@@ -1,22 +1,23 @@
 import React from "react";
 
 export default function Golden({ onGoldenClick }) {
+	
 	return (
 		<div className="mint-container">
 			<div className="div-ticket">
-				<h4 className="no-margin">limited to 20 NFTs</h4>
-				<h1 className="no-margin">Golden Ticket Tier</h1>
-				<h2 className="no-margin orange">Receive 100% more tokens</h2>
+				<h4 className="h4-desktop no-margin">limited to 20 NFTs</h4>
+				<h1 className="h1-desktop no-margin">Golden Ticket Tier</h1>
+				<h2 className="h2-desktop no-margin orange">Receive 100% more tokens</h2>
 				<div className="rounded">
-					<h1 className="no-margin">$ 50K</h1>
-					<h4 className="no-margin">payable in DAI stablecoin</h4>
+					<h1 className="h1-desktop no-margin">$ 50K</h1>
+					<h4 className="h4-desktop no-margin">payable in DAI stablecoin</h4>
 				</div>
 			</div>
 			<ul>
-				<li>Your investment counts double in the next funding round;</li>
-				<li>Access to the Seed Round Lounge;</li>
-				<li>Access to all other private lounges;</li>
-				<li>A large boost to your gamification supplies.</li>
+				<li className="li-desktop">Your investment counts double in the next funding round;</li>
+				<li className="li-desktop">Access to the Seed Round Lounge;</li>
+				<li className="li-desktop">Access to all other private lounges;</li>
+				<li className="li-desktop">A large boost to your gamification supplies.</li>
 			</ul>
 			<button className="btn-mint btn-style-orange-solid" onClick={onGoldenClick}>
 				Mint NFT
