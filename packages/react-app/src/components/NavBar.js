cb299dd3bac0351ef22afa914b8d540274cfa37d
Lorelai <nadja.rathenow@rub.de>
2022-04-21 15:58:31 +0200

    new branch

--- packages/react-app/src/components/NavBar.js
+++ packages/react-app/src/components/NavBar.js
@@ -12,6 +12,7 @@ export default function NavBar() {
 				</Link>
 			</div>
 			<div className="icon-container">
+			<h1>test</h1>
 				<a href="https://twitter.com/LifeLiveDAO">
 					<img className="nav-icon zoom" src="icons/Twitter.svg" alt="Twitter-Icon" />
 				</a>
