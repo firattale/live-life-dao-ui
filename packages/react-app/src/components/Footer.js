80064b8338e66ba654ed3ef5d9a8db31c6b036a7
Firat Tale <firat.tale@auto1.com>
2022-04-29 15:28:37 +0200

    Fix warnings

--- packages/react-app/src/components/Footer.js
+++ packages/react-app/src/components/Footer.js
@@ -5,16 +5,16 @@ export default function Footer() {
 		<footer>
 			<h5>LifeLive DAO © 2022</h5>
 			<div className="icon-container icons-footer">
-				<a className="footer-link zoom" href="mailto: contact@lifelivedao.com" target="_blank">
+				<a className="footer-link zoom" href="mailto: contact@lifelivedao.com" target="_blank" rel="noreferrer">
 					<h5>contact</h5>
 				</a>
-				<a className="zoom" href="https://twitter.com/LifeLiveDAO" target="_blank">
+				<a className="zoom" href="https://twitter.com/LifeLiveDAO" target="_blank" rel="noreferrer">
 					<img className="footer-icon" src="icons/Twitter.svg" alt="Twitter-Icon" />
 				</a>
-				<a className="zoom" href="https://t.me/lifelivedao" target="_blank">
+				<a className="zoom" href="https://t.me/lifelivedao" target="_blank" rel="noreferrer">
 					<img className="footer-icon" src="icons/Telegram.svg" alt="Telegram-Icon" />
 				</a>
-				<a className="zoom" href="https://www.instagram.com/lifelivedao/" target="_blank">
+				<a className="zoom" href="https://www.instagram.com/lifelivedao/" target="_blank" rel="noreferrer">
 					<img className="footer-icon" src="icons/Insta.svg" alt="Insta-Icon" />
 				</a>
 			</div>
--- packages/react-app/src/components/Intro.js
+++ packages/react-app/src/components/Intro.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Header } from ".";
 import { Link as ScrollLink } from "react-scroll";
 
@@ -9,14 +8,17 @@ export default function Intro() {
 			<div className="intro-container">
 				<div className="title">LifeLive DAO</div>
 				<h2>The nightlife metaverse</h2>
-				<p className="text">
-					Access virtual and hybrid events and Dance2Earn with people from all over the world.
-				</p>
+				<p className="text">Access virtual and hybrid events and Dance2Earn with people from all over the world.</p>
 				<div className="btn-main-container">
 					<ScrollLink to="mint" spy={true} smooth={true} duration={900}>
 						<button className="btn btn-style-blue-solid zoom">Buy Investor NFTs</button>
 					</ScrollLink>
-					<a className="link" href="https://drive.google.com/file/d/1xs-bvwmNUumlscIphOXIIbnsazDGbNrw/view" target="_blank">
+					<a
+						className="link"
+						href="https://drive.google.com/file/d/1xs-bvwmNUumlscIphOXIIbnsazDGbNrw/view"
+						target="_blank"
+						rel="noreferrer"
+					>
 						<button className="btn btn-style-blue-light btn-margin zoom">Download litepaper</button>
 					</a>
 				</div>
