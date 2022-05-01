98280a0c55c91fd27e862ecbc50d0c6a05d807eb
Firat Tale <firat.tale@auto1.com>
2022-05-01 16:42:14 +0200

    Show video on desktop

--- packages/react-app/src/components/Intro.js
+++ packages/react-app/src/components/Intro.js
@@ -1,28 +1,25 @@
 import React from "react";
-import { Header } from ".";
 import { Link as ScrollLink } from "react-scroll";
 
 export default function Intro() {
 	return (
-		<Header>
-			<div className="intro-container">
-				<div className="title">LifeLive DAO</div>
-				<h2>The nightlife metaverse</h2>
-				<p className="text">Access virtual and hybrid events and Dance2Earn with people from all over the world.</p>
-				<div className="btn-main-container">
-					<ScrollLink to="mint" spy={true} smooth={true} duration={900}>
-						<button className="btn btn-style-blue-solid zoom">Buy Investor NFTs</button>
-					</ScrollLink>
-					<a
-						className="link"
-						href="https://drive.google.com/file/d/1xs-bvwmNUumlscIphOXIIbnsazDGbNrw/view"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<button className="btn btn-style-blue-light btn-margin zoom">Download litepaper</button>
-					</a>
-				</div>
+		<div className="intro-container">
+			<div className="title">LifeLive DAO</div>
+			<h2>The nightlife metaverse</h2>
+			<p className="text">Access virtual and hybrid events and Dance2Earn with people from all over the world.</p>
+			<div className="btn-main-container">
+				<ScrollLink to="mint" spy={true} smooth={true} duration={900}>
+					<button className="btn btn-style-blue-solid zoom">Buy Investor NFTs</button>
+				</ScrollLink>
+				<a
+					className="link"
+					href="https://drive.google.com/file/d/1xs-bvwmNUumlscIphOXIIbnsazDGbNrw/view"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button className="btn btn-style-blue-light btn-margin zoom">Download litepaper</button>
+				</a>
 			</div>
-		</Header>
+		</div>
 	);
 }
--- packages/react-app/src/pages/MainPage/index.js
+++ packages/react-app/src/pages/MainPage/index.js
@@ -8,13 +8,20 @@ import Features from "../../components/Features";
 import InvestorNfts from "../../components/InvestorNfts";
 import Impressum from "../../components/Impressum";
 import { MintPage } from "../../pages/MintPage";
+import videoBG from "../../background/hero-video.mp4";
+import bgPoster from "../../background/bg-hero.png";
 
 export const MainPage = () => {
 	return (
 		<Body>
 			<div className="bg-hero">
-				<NavBar />
-				<Intro />
+				<video loop autoPlay muted poster={bgPoster}>
+					<source src={videoBG} type="video/mp4" />
+				</video>
+				<div>
+					<NavBar />
+					<Intro />
+				</div>
 			</div>
 			<Project />
 			<Features />
