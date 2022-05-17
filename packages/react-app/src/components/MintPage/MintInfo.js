765d93cd29135177245f3018ff2b401d55054257
Firat Tale <firat.tale@auto1.com>
2022-05-17 15:55:21 +0200

    update mintinfo

--- packages/react-app/src/components/MintPage/MintInfo.js
+++ packages/react-app/src/components/MintPage/MintInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MintInfo() {
 	return (
-		<div className="">
+		<div className="text side-padding mint-info mint-weight">
 			Please DO NOT send funds from an exchange.<br></br>The wallet address you use to purchase your ticket(s) will be
 			registered as the future recipient of the LifeLive tokens.
 		</div>
