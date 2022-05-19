daa5c982d7ce9316a3ce7e57a57127b8222ff94a
Firat Tale <firat.tale@auto1.com>
2022-05-19 22:24:06 +0200

    add wallet connect

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -4,10 +4,21 @@ import { useEthers, ChainId } from "@usedapp/core";
 import { DialogWarning } from "./DialogWarning";
 import { dialogContentNoMetamask, dialogWrongNetwork } from "../constants";
 import toast from "react-hot-toast";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export const WalletButton = () => {
-	const { account, activateBrowserWallet, deactivate, chainId, switchNetwork } = useEthers();
-
+	const { account, activateBrowserWallet, activate, deactivate, chainId, switchNetwork } = useEthers();
+	const onConnectWalletConnect = async () => {
+		try {
+			const provider = new WalletConnectProvider({
+				infuraId: "7a63d05a74a34e578282178c3b0d4c9f",
+			});
+			await provider.enable();
+			await activate(provider);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	const [rendered, setRendered] = React.useState("");
 	const [content, setContent] = React.useState("");
 	const [openDialog, setOpenDialog] = React.useState(false);
@@ -42,7 +53,7 @@ export const WalletButton = () => {
 		}
 
 		if (!account) {
-			activateBrowserWallet();
+			onConnectWalletConnect();
 			toast("Wallet connected", {
 				icon: "🤝",
 			});
